fd94e665cd7a809696f3de5114f7ef2a13892796
GalaxyBook2 <manamana32321@gmail.com>
2024-08-18 18:21:20 +0900

    feat: extend BaseUser class

--- user/models.py
+++ user/models.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
 
@@ -7,10 +12,15 @@ class Department(models.Model):
         ("PERFORMANCE", "공연부"),
         ("PROMOTION", "홍보부"),
         ("PLANNING", "기획부"),
-        ("INSTRUMENT", "악기부")
+        ("INSTRUMENT", "악기부"),
     ]
     name = models.CharField(max_length=10)
-    leader = models.ForeignKey('CustomUser', null=True, on_delete=models.SET_NULL, related_name="leading_departments")
+    leader = models.ForeignKey(
+        "CustomUser",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="leading_departments",
+    )
 
 
 class PositionChoices(Enum):
@@ -24,15 +34,53 @@ class PositionChoices(Enum):
         return [(choice.name, choice.value) for choice in cls]
 
 
-class CustomUser(models.Model):
-    name = models.CharField(max_length=40)
-    nickname = models.CharField(max_length=40)
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, nickname, password):
+        if not email:
+            raise ValueError("must have user email")
+        user = self.model(email=self.normalize_email(email), nickname=nickname)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email=self.normalize_email(email), nickname=nickname, password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20)
     bio = models.TextField()
     profile_image = models.ImageField()
-    generation = models.ForeignKey('core.Generation', on_delete=models.PROTECT)
-    session = models.ForeignKey('core.Session', on_delete=models.PROTECT)
+    generation = models.ForeignKey("core.Generation", on_delete=models.PROTECT)
+    session = models.ForeignKey("core.Session", on_delete=models.PROTECT)
     position = models.CharField(max_length=30, choices=PositionChoices.choices())
     department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    created_datetime = models.DateTimeField(auto_created=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
 
     @property
     def is_amang_staff(self):
