d51abfa067c9148d23874914a97896ced2a9f336
GalaxyBook2 <manamana32321@gmail.com>
2024-09-10 02:41:26 +0900

    fix: add eb env load logic

--- management/commands/create_superuser.py
+++ management/commands/create_superuser.py
@@ -1,12 +1,15 @@
 import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from dotenv import load_dotenv
 
 
 class Command(BaseCommand):
     help = "Create a superuser if none exists"
 
     def handle(self, *args, **kwargs):
+        load_dotenv("/opt/elasticbeanstalk/deployment/env")
+
         User = get_user_model()
         email = os.getenv("DJANGO_SUPERUSER_EMAIL")
         password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
