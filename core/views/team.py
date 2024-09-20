6b0202bf45e3a69bb111e30a5ae96c9d2d5f1384
GalaxyBook2 <manamana32321@gmail.com>
2024-09-20 15:59:42 +0900

    feat: add Team unapply

--- core/views/team.py
+++ core/views/team.py
@@ -4,6 +4,7 @@ from rest_framework.generics import (
     GenericAPIView,
 )
 from rest_framework.response import Response
+from rest_framework import status
 
 from core.models.session import Session
 from core.models.team import Team
@@ -37,11 +38,34 @@ class TeamApplyAPIView(GenericAPIView):
         newMembers = list(oldMemberList)
         if requestUser not in oldMemberList:
             newMembers.append(requestUser)
+        else:
+            return Response(
+                {"detail": "세션에 이미 등록된 유저입니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         ms.members.set(newMembers)
         ms.save()
 
         serializer = self.serializer_class(team)
         return Response(serializer.data)
 
-    def delete(self, *args, **kwargs):
-        raise NotImplementedError()
+    def delete(self, request, **kwargs):
+        # TODO: requestUser = JWT 토큰에서 읽어야 함
+        requestUser = CustomUser.objects.all()[0]  # 임시 유저
+        team = self.get_object()
+        session = Session.objects.get(name=request.data.get("session"))
+        ms = team.memberSessions.get(session=session)
+        oldMemberList = ms.members.all()
+        newMembers = list(oldMemberList)
+        if requestUser in oldMemberList:
+            newMembers.remove(requestUser)
+        else:
+            return Response(
+                {"detail": "세션에 등록되지 않은 유저입니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        ms.members.set(newMembers)
+        ms.save()
+
+        serializer = self.serializer_class(team)
+        return Response(serializer.data)
