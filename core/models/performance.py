dbed186c443627549871aaf2e713c8f0962dabdb
GalaxyBook2 <manamana32321@gmail.com>
2024-09-22 05:50:31 +0900

    chore: modify Performance model

--- core/models/performance.py
+++ core/models/performance.py
@@ -5,9 +5,9 @@ class Performance(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     representativeImage = models.ImageField(upload_to="performance/", null=True)
-    location = models.CharField(max_length=255)
-    startDatetime = models.DateTimeField()
-    endDatetime = models.DateTimeField()
+    location = models.CharField(max_length=255, blank=True, null=True)
+    startDatetime = models.DateTimeField(blank=True, null=True)
+    endDatetime = models.DateTimeField(blank=True, null=True)
 
     createdDatetime = models.DateTimeField(auto_now_add=True)
     updatedDatetime = models.DateTimeField(auto_now=True)
