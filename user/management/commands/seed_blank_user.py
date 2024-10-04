17feb38c9346880b020f06a2380b79a32269e933
GalaxyBook2 <manamana32321@gmail.com>
2024-10-04 20:55:27 +0900

    chore: Add error handling checking code for debug

--- user/management/commands/seed_blank_user.py
+++ user/management/commands/seed_blank_user.py
@@ -7,22 +7,28 @@ class Command(BaseCommand):
     help = "Create a blank user with id = 0 if not exists"
 
     def handle(self, *args, **kwargs):
-        User = get_user_model()
-        email = User.BLANK_USER_EMAIL
-        random_password = str(uuid.uuid4())
-        random_nickname = self.generate_unique_random_nickname()
+        try:
+            User = get_user_model()
+            email = User.BLANK_USER_EMAIL
+            random_password = str(uuid.uuid4())
+            random_nickname = self.generate_unique_random_nickname()
 
-        if not User.objects.filter(email=email).exists():
-            User.objects.create(
-                email=email, password=random_password, nickname=random_nickname
+            if not User.objects.filter(email=email).exists():
+                User.objects.create(
+                    email=email, password=random_password, nickname=random_nickname
+                )
+                self.stdout.write(
+                    self.style.SUCCESS(f"Blank User {email} created successfully.")
+                )
+            else:
+                self.stdout.write(
+                    self.style.WARNING(f"Blank User with email {email} already exists.")
+                )
+        except Exception as e:
+            self.style.ERROR(
+                f"Error while creating blank user with {User.BLANK_USER_EMAIL}"
             )
-            self.stdout.write(
-                self.style.SUCCESS(f"Blank User {email} created successfully.")
-            )
-        else:
-            self.stdout.write(
-                self.style.WARNING(f"Blank User with email {email} already exists.")
-            )
+            raise e
 
     @classmethod
     def generate_unique_random_nickname(cls):
