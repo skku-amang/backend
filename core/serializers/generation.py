f7054e21830e5ccb8bb5de924977a19db68fd8d9
nhjbest22 <nhjbest22@g.skku.edu>
2024-10-04 15:40:38 +0900

    hotfix: fix typo in seed_data and type_error in validate_order

--- core/serializers/generation.py
+++ core/serializers/generation.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 from ..models.generation import Generation
 
@@ -10,6 +11,6 @@ class GenerationSerializer(serializers.ModelSerializer):
 
     def validate_order(self, value):
         # 소수점 부분이 0 혹은 5인지 확인
-        if value % 0.5 != 0:
+        if value % Decimal(0.5) != 0:
             raise serializers.ValidationError("Order 값은 0.5 단위로 입력해주세요.")
         return value
