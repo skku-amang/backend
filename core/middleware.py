37e7046602bff1eb2bf55004cccf717e25be55b7
GalaxyBook2 <manamana32321@gmail.com>
2024-10-06 02:59:16 +0900

    chore: Change block duration

--- core/middleware.py
+++ core/middleware.py
@@ -9,14 +9,14 @@ class BlockIPMiddleware:
     def __call__(self, request):
         ip = request.META.get("REMOTE_ADDR")
         if cache.get(ip):
-            return HttpResponseForbidden("Your IP has been permanently blocked")
+            return HttpResponseForbidden("blocked")
 
         response = self.get_response(request)
 
         if response.status_code == 404 and request.path.endswith(".php"):
             attempts = cache.get(ip, 0) + 1
-            cache.set(ip, attempts, timeout=3600)  # 1시간 동안 유지
+            cache.set(ip, attempts, timeout=36000)  # 10시간 동안 유지
             if attempts > 5:  # 5번 이상 404 에러 발생 시 차단
-                cache.set(ip, True, timeout=3600)  # 1시간 동안 차단
+                cache.set(ip, True, timeout=36000)  # 10시간 동안 차단
 
         return response
