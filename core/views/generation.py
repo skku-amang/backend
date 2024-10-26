0f88a48869fc9b05fc3dac019463180a3f540a8b
nhjbest22 <nhjbest22@g.skku.edu>
2024-10-27 02:40:29 +0900

    fix: change exceptions from Django exception to DRF exception

--- core/views/generation.py
+++ core/views/generation.py
@@ -1,7 +1,8 @@
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 from django.http import Http404
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.exceptions import ValidationError, NotFound
 
 from core.models.generation import Generation
 from core.serializers.generation import GenerationSerializer
@@ -31,15 +32,11 @@ class GenerationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     def get_object(self):
         pk = self.kwargs.get('pk')
         try:
-            # pk 값을 Decimal로 변환하여 조회
             decimal_pk = Decimal(pk)
-        except Exception:
-            # Decimal 변환에 실패하면 404 오류 반환
-            raise Http404("Invalid Generation ID")
+        except InvalidOperation:
+            raise ValidationError("Invalid Generation ID")
 
-        # Decimal 값으로 Generation을 조회
         try:
             return Generation.objects.get(order=decimal_pk)
         except Generation.DoesNotExist:
-            # 객체를 찾지 못하면 404 오류 반환
-            raise Http404("Generation not found")
+            raise NotFound("Generation does not exist")
