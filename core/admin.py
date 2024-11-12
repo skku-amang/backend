34668596e85af505480c7b3db72488f77597fd90
GalaxyBook2 <manamana32321@gmail.com>
2024-11-12 15:49:51 +0900

    chore: add feedback-related models to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 from core.models.generation import Generation
+from core.models.feedback import Feedback, FeedbackQuestion, FeedbackAnswer
 
 
-admin.site.register(Generation)
+models = [Generation, Feedback, FeedbackQuestion, FeedbackAnswer]
+
+(admin.site.register(model) for model in models)
